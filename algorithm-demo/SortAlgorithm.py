1ce630a6eb6d46326ebc8ce5488c66e578c29af2
Blushyes <2671431017@qq.com>
2023-10-11 22:41:44 +0800

    优化变量名

--- algorithm-demo/SortAlgorithm.py
+++ algorithm-demo/SortAlgorithm.py
@@ -3,20 +3,21 @@ from manimlib import *
 # 交换一次所需要的时间
 MOVE_TIME = 1
 
+
 # 排序算法演示
 class SortAlgorithm(object):
-    def __init__(self, arr, scene : Scene, color=RED, width=0.2, move_time=0.1, buff=0.1, scan=False):
+    def __init__(self, arr, scene: Scene, color=RED, width=0.2, move_time=0.1, buff=0.1, scan=False):
         self._run_time = move_time
         self._width = width
         self._color = color
         self._buff = buff
         self._scan = scan
-        self.__size = 0.7
-        self.scene = scene
-        self.arr = list(arr)
+        self._size = 0.7
+        self._scene = scene
+        self._arr = list(arr)
         self._max = max(arr)
-        self.group = self._group()
-        scene.add(self.group)
+        self._group = self._group()
+        scene.add(self._group)
 
     # 设置柱体属性
     def set_pillar(self, color):
@@ -24,7 +25,7 @@ class SortAlgorithm(object):
 
     # 按索引设置柱体颜色，有些排序可能会用到，比如说快速排序标出哨兵
     def set_color(self, index, color):
-        self.group[index].set_fill(color=color)
+        self._group[index].set_fill(color=color)
 
     # 设置柱体宽度
     def set_width(self, width):
@@ -36,7 +37,7 @@ class SortAlgorithm(object):
 
     # 设置arr
     def set_arr(self, arr):
-        self.arr = list(arr)
+        self._arr = list(arr)
 
     # 生成一个排序柱
     def _pillar(self, height):
@@ -49,49 +50,52 @@ class SortAlgorithm(object):
     # 根据arr创建一个VGroup
     def _group(self):
         res = VGroup()
-        res.add(*[self._pillar(block) for block in self.arr])
+        res.add(*[self._pillar(block) for block in self._arr])
         res.arrange(RIGHT, aligned_edge=DOWN, buff=self._buff)
         return res
 
     # 交换
     def _exchange(self, a, b):
         if self._scan:
-            self.group[a].set_fill(color=YELLOW)
-            self.group[b].set_fill(color=YELLOW)
-        
-        self.scene.play(self.group[a].animate.shift((self.group[b].get_x() - self.group[a].get_x()) * RIGHT), self.group[b].animate.shift((self.group[a].get_x() - self.group[b].get_x()) * RIGHT), run_time=self._run_time)
+            self._group[a].set_fill(color=YELLOW)
+            self._group[b].set_fill(color=YELLOW)
+
+        self._scene.play(self._group[a].animate.shift((self._group[b].get_x() - self._group[a].get_x()) * RIGHT),
+                         self._group[b].animate.shift((self._group[a].get_x() - self._group[b].get_x()) * RIGHT),
+                         run_time=self._run_time)
 
     # 获取所有的柱体
     def get_group(self):
-        return self.group
+        return self._group
 
     # 交换后更新
     def update(self, arr):
         res = None
         for i in range(len(arr)):
-            if self.arr[i] == arr[i]:
+            if self._arr[i] == arr[i]:
                 continue
             if res is None:
                 res = i
             else:
                 self._exchange(res, i)
-                self.arr[res], self.arr[i] = self.arr[i], self.arr[res]
-                self.scene.remove(self.group)
-                self.group = self._group()
-                self.scene.add(self.group)
+                self._arr[res], self._arr[i] = self._arr[i], self._arr[res]
+                self._scene.remove(self._group)
+                self._group = self._group()
+                self._scene.add(self._group)
                 break
 
     # 静态更新
     def static_update(self, arr):
         for i in range(len(arr)):
-            if self.arr[i] == arr[i]:
+            if self._arr[i] == arr[i]:
                 continue
 
-            self.arr[i] = arr[i]
+            self._arr[i] = arr[i]
             tmp = self._group()
-            self.scene.play(FadeTransform(self.group, tmp), run_time=self._run_time)
-            self.group = tmp
+            self._scene.play(FadeTransform(self._group, tmp), run_time=self._run_time)
+            self._group = tmp
             break
+
 
 # 用选择排序举例
 def select_sort(arr, sa):
